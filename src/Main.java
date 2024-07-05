40cb61331d9b22c6c5e2fa28306fd8dd8f4a17e2
thomas <tomer157@gmail.com>
2024-07-05 05:17:27 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("Decrypted: " + decryptedDataFromIntArray);
 
         // Decrypt encrypted numbers array with a key length
-        String extractedKeys[] = Encrypt.breakXOREncryption(encryptedDataFromTxt, key.length());
+        String[] extractedKeys = Encrypt.breakXOREncryption(encryptedDataFromTxt, key.length());
         System.out.println("Decrypt by key length to original string: " + Arrays.asList(extractedKeys));
 
         // Sanity test cases
@@ -77,17 +77,14 @@ public class Main {
     }
 
     private static void test3() {
-        // should failed its a negative test to validate correct input data
+        // negative test
         String inputString = "";
         String key = "abc";
-        int[] expectedData = new int[]{53, 10, 10, 18, 66, 10, 18, 66, 2, 65, 21, 12, 15, 6, 6, 19, 66, 20, 14, 16, 15, 5, 66, 19, 13, 7, 2, 18, 7, 67, 12, 3, 8, 4, 66, 10, 21, 66, 16, 0, 4, 6};
 
         // Encrypt the text with a string key .
         int[] encryptedDataFromTxt = Encrypt.xorEncrypt(inputString, key);
 
-
         try {
-            // Check if shouldnt equals expectedData
             if (encryptedDataFromTxt[0] == -1) {
                 throw new AssertionError("test 3 passed");
             }
@@ -112,7 +109,7 @@ public class Main {
             String decryptedDataFromIntArray = Encrypt.xorDecrypt(expectedData, key);
 
             // Check if encryptedDataFromTxt equals expectedData.
-            if (!decryptedDataFromIntArray.equals(inputStringData) || decryptedDataFromIntArray.equals("-1")) {
+            if (!decryptedDataFromIntArray.equals(inputStringData)) {
                 throw new AssertionError("test 4 passed");
             }
 
@@ -125,7 +122,7 @@ public class Main {
 
     private static void test5() {
         try {
-            String extractedKeys[] = Encrypt.breakXOREncryption(new int[]{}, 4);
+            String[] extractedKeys = Encrypt.breakXOREncryption(new int[]{}, 4);
             if (extractedKeys[0].equals("-1")) {
                 System.out.println("test 5 passed");
             } else {
